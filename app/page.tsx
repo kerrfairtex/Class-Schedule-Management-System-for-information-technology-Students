dbf0525bbcb789865a48d125175487fa4d5a848d
Kerr Fairtex <kerrfairtex@gmail.com>
2026-08-14 14:22:22 +0800

    clear description

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link';
 import { Calendar, GraduationCap, Shield, Users, MapPin, BookOpen, Building2 } from 'lucide-react';
 import { ORGANIZATION } from '../lib/domain/constants';
