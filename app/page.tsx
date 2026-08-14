5aa3c10dda3afaa435433b143f80ce43dc28a013
Kerr Fairtex <kerrfairtex@gmail.com>
2026-08-14 16:42:32 +0800

    remove unused ClassScheduleForm import from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import ClassScheduleForm from '@/components/ClassScheduleForm';
 import Link from 'next/link';
 import { Calendar, GraduationCap, Shield, Users, MapPin, BookOpen, Building2 } from 'lucide-react';
 import { ORGANIZATION } from '../lib/domain/constants';
